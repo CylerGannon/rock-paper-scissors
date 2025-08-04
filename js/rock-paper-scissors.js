21f696eec7bb116a9ebf6036422df83035c04828
Cyler Gannon <57932006+CylerGannon@users.noreply.github.com>
2025-08-04 14:42:42 -0500

    Wrote function to get the computer choice
    This function randomly returns either rock, paper, or scissors in string format.

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+const min = 0;
+const max = 2;
+
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+
+const randomInt = getRandomIntInclusive(0, 2);
+
+if (randomInt == rock) {
+    return "rock";
+} else {
+    if (randomInt == paper) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
