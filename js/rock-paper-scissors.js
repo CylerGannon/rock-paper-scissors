7ae7fb28e27a8b96a1d5bd63277f9e8abf6eb181
Cyler Gannon <57932006+CylerGannon@users.noreply.github.com>
2025-08-04 14:45:48 -0500

    Named function getComputerChoice()
    The previous commit was functional, however the structure was not correct.

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -4,21 +4,24 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
 }
 
-const min = 0;
-const max = 2;
+function getComputerChoice () {
+    const min = 0;
+    const max = 2;
 
-const rock = 0;
-const paper = 1;
-const scissors = 2;
+    const rock = 0;
+    const paper = 1;
+    const scissors = 2;
 
-const randomInt = getRandomIntInclusive(0, 2);
+    const randomInt = getRandomIntInclusive(0, 2);
 
-if (randomInt == rock) {
-    return "rock";
-} else {
-    if (randomInt == paper) {
-        return "paper";
+    if (randomInt == rock) {
+        return "rock";
     } else {
-        return "scissors";
+        if (randomInt == paper) {
+            return "paper";
+        } else {
+            return "scissors";
+        }
     }
 }
+
